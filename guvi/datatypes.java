de18344f8daa260079278c1fb7e68c800713c704
Gnanapradeepa <34647153+Gnanapradeepa@users.noreply.github.com>
2018-03-09 15:48:47 +0530

    Create datatypes.java

---
+++ guvi/datatypes.java
@@ -0,0 +1,21 @@
+package pradee;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String name="pradee";
+		String city="coimbatore";
+		int age=20;
+		float height=5.1f;
+		boolean married=false;
+		char sex='f';
+		double pop=4828853;
+		System.out.println("name:" +name +"\ncity:" +city +"\nage:" +age +"\nheight:" +height +"\nmarried:" +married +"\nsex:" +sex +"\npopuplation:" +pop);
+		
+		
+	}
+
+}
